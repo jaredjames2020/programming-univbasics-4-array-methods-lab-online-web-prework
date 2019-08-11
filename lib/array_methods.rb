b554fe8a6f2ee9a31ce84cb21cfc8504af2fc7c1
munificent-commit-7390 <jaredjames2020@gmail.com>
2019-08-11 18:45:19 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -17,11 +17,12 @@ using_reverse(["wow", "I", "am", "really", "learning", "arrays!"])
 def using_first(array)
   array.first
 end
-
+using_first(["wow", "I", "am", "really", "learning", "arrays!"])
 
 def using_last(array)
   array.last
 end
+using_last(["wow", "I", "am", "really", "learning", "arrays!"])
 
 def using_size(array)
   array.size
