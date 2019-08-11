b7487c3376cd71130cf0a748ef90c7450e373186
munificent-commit-7390 <jaredjames2020@gmail.com>
2019-08-11 18:44:13 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,7 +2,7 @@ def using_include(array, element)
   array.include?(element)
 end
 using_include(["wow", "I", "am", "really", "learning", "arrays!"], "wow")
-using_include(["wow", "I", "am", "really", "learning", "arrays!"], "wow")
+using_include(["wow", "I", "am", "really", "learning", "arrays!"], "Tardis")
 
 def using_sort(array)
   array.sort
